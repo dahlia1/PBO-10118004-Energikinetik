fd57b860d2d1f49143534e755bebf55d26745a9b
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-10-17 20:48:05 +0700

    Add files via upload

---
+++ PBO-10118004-Energikinetik/PBO-10118004-Energikinetik/src/pbo/pkg10118004/energikinetik/PBO10118004Energikinetik.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118004.energikinetik;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public class PBO10118004Energikinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      energi kinetik = new energi();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("=====PROGRAM MENCARI ENERGI KINETIK=====");
+        System.out.println("Energi Kinetik\t : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha\t\t : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
+
+    
+
